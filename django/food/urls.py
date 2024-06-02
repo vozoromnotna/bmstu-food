2fb50828e559fba19d0bb985d3de24cf110758cc
vozoromnotna <anton20012010@gmail.com>
2024-06-02 15:23:13 +0300

    base registration

--- django/food/urls.py
+++ django/food/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 
+app_name = 'food'
+
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path('registration', UserRegistrationFromView.as_view(), name='registration'),
+    path('registr_done', UserRegistrationDoneView.as_view(), name='registration_done')
 ]
--- django/food/views.py
+++ django/food/views.py
@@ -1,6 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, FormView
+from .forms import *
 class IndexView(TemplateView):
     template_name = "food/index.html"
 
+class UserRegistrationDoneView(TemplateView):
+    template_name = "registration/registration_done.html"
+
+class UserRegistrationFromView(FormView):
+    template_name = "registration/registration.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('food:registration_done')
+    
+    def form_valid(self, form:UserRegistrationForm):
+        user:User = form.instance
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        return super().form_valid(form)
