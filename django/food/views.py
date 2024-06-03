76e0077a5a609bb55ce14b3581e831009e27ac27
vozoromnotna <anton20012010@gmail.com>
2024-06-03 16:49:42 +0300

    side menu

--- django/food/views.py
+++ django/food/views.py
@@ -1,10 +1,12 @@
+from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView, FormView, ListView
 from django.contrib.auth.views import LoginView
-from .models import Order, OrderDetails
+from .models import Order, OrderDetails, FavoriteDish, Dish
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum, F, DecimalField
 
 from .forms import *
 class IndexView(TemplateView):
@@ -30,26 +32,19 @@ class UserLoginFormView(LoginView):
     
 
 class UserOrdersView(LoginRequiredMixin, ListView):
-    template_name = "food/orders_view.html"
+    template_name = "food/orders.html"
     model = Order
     context_object_name = "orders"
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user).prefetch_related('orderdetails_set')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['order_details'] = OrderDetails.objects.filter(order__user=self.request.user).select_related('dish')
-        context['']
-#         Нужно добавить суммы
-#         SELECT food_order.id, SUM(count * price)
-#         FROM food_order
-#         JOIN
-#         food_orderdetails
-#         ON food_order.id = food_orderdetails.order_id
-#         JOIN
-#         food_dish
-#         ON food_orderdetails.dish_id = food_dish.id
-#         GROUP BY food_order.id
-        return context
+        return Order.objects.filter(user=self.request.user).prefetch_related('orderdetails_set').annotate(
+            total_cost=Sum(F('orderdetails__count') * F('orderdetails__dish__price'), output_field=DecimalField())
+        )
     
+class UserFavoriteDishView(LoginRequiredMixin, ListView):
+    template_name = "food/favorite_dish.html"
+    model = FavoriteDish
+    context_object_name = "favorite_dishes"
+
+    def get_queryset(self):
+        return FavoriteDish.objects.filter(user=self.request.user)
