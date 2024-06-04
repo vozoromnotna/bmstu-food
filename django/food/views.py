c4edbbc77b0c857003a3fda3fc378a062eb02521
vozoromnotna <anton20012010@gmail.com>
2024-06-04 11:37:15 +0300

    workers view delete

--- django/food/views.py
+++ django/food/views.py
@@ -1,6 +1,6 @@
 from django.db.models.query import QuerySet
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView, FormView, ListView, DeleteView
 from django.core.exceptions import PermissionDenied
@@ -70,3 +70,45 @@ class WorkerFoodservicesView(LoginRequiredMixin, ListView):
         if not user.groups.filter(name="workers").exists():
             raise PermissionDenied
         return Foodservice.objects.filter(foodserviceworker__worker=user)
+    
+class FoodserviceWorkersView(LoginRequiredMixin, ListView):
+    model = User
+    context_object_name = "workers"
+    template_name = "food/foodservice_workers.html"
+    
+    def get_queryset(self):
+        foodservice = Foodservice.objects.get(title=self.kwargs["title"])
+        if self.request.user != foodservice.owner:
+            raise PermissionDenied
+        return User.objects.filter(foodserviceworker__foodservice=foodservice)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title_2'] = self.kwargs['title'] #Иначе шаблон не видит title
+        return context
+    
+    
+class FoodserviceWorkerDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "food/foodservice_workers_delete.html"
+    context_object_name = "foodservice_worker"
+    model = FoodserviceWorker
+    def form_valid(self, form):
+        
+        if self.request.user != self.object.foodservice.owner:
+            raise PermissionDenied
+
+        self.object.delete()
+        
+        success_url = self.get_success_url()
+        return HttpResponseRedirect(success_url)
+
+    def get_object(self):
+        foodservice_title = self.kwargs["title"]
+        user_id = self.kwargs["user_id"]
+        return FoodserviceWorker.objects.get(foodservice__title=foodservice_title, worker__id=user_id)
+    
+    
+    def get_success_url(self):
+        return reverse_lazy("food:foodservice_workers", kwargs = {"title": self.kwargs["title"]})
+
+    
